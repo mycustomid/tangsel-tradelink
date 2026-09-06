cb46950f91ff6803df241921a99ab22af7275057
mycustomid <myaicustom@gmail.com>
2026-09-06 23:14:57 +0700

    deploy: configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://mycustomid.github.io',
+  base: '/tangsel-tradelink',
   output: 'static',
   trailingSlash: 'never',
 });
